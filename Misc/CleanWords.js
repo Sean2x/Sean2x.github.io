2be3084995e7c4a9d60e84627e1dc5238704acdb
Sean2x <74382761+Sean2x@users.noreply.github.com>
2026-03-18 13:55:47 +0000

    s

--- Misc/CleanWords.js
+++ Misc/CleanWords.js
@@ -17,7 +17,20 @@ const cleaned = words.filter(word => {
     w.length >= 3 &&
     w.length <= 16 &&
     !w.includes("'") &&
-    !w.includes("-")
+    !w.includes("-") &&
+    !w.includes(".") &&
+    !w.includes("&") 
+    &&
+    !w.includes("1") &&
+    !w.includes("2") &&
+    !w.includes("3") &&
+    !w.includes("4") &&
+    !w.includes("5") &&
+    !w.includes("6") &&
+    !w.includes("7") &&
+    !w.includes("8") &&
+    !w.includes("9") &&
+    !w.includes("0")
   );
 });
 
