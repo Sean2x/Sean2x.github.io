176d71c7016e474c2e3b11eac276b5da8da653db
Sean2x <seanlirazan@gmail.com>
2026-03-28 19:48:55 -0500

    a

--- LexiconScript.js
+++ LexiconScript.js
@@ -346,6 +346,13 @@ shuffleButton.addEventListener("click", () => {
     btn.textContent = letters[idx];
     btn.disabled = false; // reactivate all letters
     btn.classList.toggle("qu", letters[idx] === "QU");
+
+    const orb = btn.querySelector(".score-orb");
+
+    const value =
+      letter === "QU" ? letterValues["Q"] : letterValues[letter] || 0;
+
+    orb.style.backgroundColor = getScoreColor(value);
   });
 
   // Clear word bar and usedButtons
