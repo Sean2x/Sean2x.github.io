e58492b5029579430e5dc2d5f8e5f7ab703d3069
Sean2x <74382761+Sean2x@users.noreply.github.com>
2026-04-22 15:19:50 +0000

    s

--- FishGame.js
+++ FishGame.js
@@ -548,7 +548,7 @@ function animate() {
   // CPU per frame
   const cpu = performance.now() - start;
 
-  versionEl.textContent = `v2.1 | FPS:${fps} | Fish:${fishes.length} | CPU:${cpu.toFixed(1)}ms | dt:${(dt * 1000).toFixed(1)}ms`;
+  // versionEl.textContent = `v2.1 | FPS:${fps} | Fish:${fishes.length} | CPU:${cpu.toFixed(1)}ms | dt:${(dt * 1000).toFixed(1)}ms`;
 
   requestAnimationFrame(animate);
 }
